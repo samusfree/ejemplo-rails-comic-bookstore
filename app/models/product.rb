17953743fcaecb1a9d563b9098ebfdec989ec142
Julio Lopez Montalvo <ljuliom@gmail.com>
2014-07-24 22:59:01 -0500

    validations product

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,2 +1,9 @@
 class Product < ActiveRecord::Base
+	validates :title, :description, :image_url, presence: true
+	validates :price, numericality: {greater_than_or_equal: 0.01}
+	validates :title, uniqueness: true, length: { minimum: 10, too_short: "%{count} characters is the minimun allowed" }
+	validates :image_url, allow_blank: true, format: {
+																						with: %r{\.(gif|jpg|png)\Z}i,
+																						message: 'must be a URL for GIF, JPG or PNG image.'
+																						}
 end
