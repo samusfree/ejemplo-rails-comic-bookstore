e200480e044f12c7d8aec96eb06cccb09d2be152
Julio Lopez Montalvo <ljuliom@gmail.com>
2014-07-25 01:03:46 -0500

    test controller products

--- test/controllers/products_controller_test.rb
+++ test/controllers/products_controller_test.rb
@@ -3,6 +3,12 @@ require 'test_helper'
 class ProductsControllerTest < ActionController::TestCase
   setup do
     @product = products(:one)
+    @update = {
+      title: 'Lorem Lipsum',
+      description: 'Wiblles are fun!',
+      image_url: 'lorem.jpg',
+      price: 19.95
+    }
   end
 
   test "should get index" do
@@ -18,7 +24,7 @@ class ProductsControllerTest < ActionController::TestCase
 
   test "should create product" do
     assert_difference('Product.count') do
-      post :create, product: { description: @product.description, image_url: @product.image_url, price: @product.price, title: @product.title }
+      post :create, product: @update
     end
 
     assert_redirected_to product_path(assigns(:product))
@@ -35,7 +41,7 @@ class ProductsControllerTest < ActionController::TestCase
   end
 
   test "should update product" do
-    patch :update, id: @product, product: { description: @product.description, image_url: @product.image_url, price: @product.price, title: @product.title }
+    patch :update, id: @product, product: @update
     assert_redirected_to product_path(assigns(:product))
   end
 
@@ -45,5 +51,12 @@ class ProductsControllerTest < ActionController::TestCase
     end
 
     assert_redirected_to products_path
+  end
+
+  test "there is a form and submit button in new path" do
+    get :new
+    assert_response :success
+    assert_select "form", 1
+    assert_select 'input[type="submit"]', 1
   end
 end
