2b842e0d374710718c79a333e818a444fd3d69e8
Julio Lopez Montalvo <ljuliom@gmail.com>
2014-07-25 00:57:30 -0500

    test model product

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,6 +1,6 @@
 class Product < ActiveRecord::Base
-	validates :title, :description, :image_url, presence: true
-	validates :price, numericality: {greater_than_or_equal: 0.01}
+	validates :title, :description, :image_url, :price, presence: true
+	validates :price, numericality: { greater_than_or_equal_to: 0.01 }
 	validates :title, uniqueness: true, length: { minimum: 10, too_short: "%{count} characters is the minimun allowed" }
 	validates :image_url, allow_blank: true, format: {
 																						with: %r{\.(gif|jpg|png)\Z}i,
