b6d48328fcfd19a97c280f138b08c9f822b2df31
Julio Lopez Montalvo <ljuliom@gmail.com>
2014-07-24 23:21:20 -0500

    relations cart, product with lineitems

--- app/models/product.rb
+++ app/models/product.rb
@@ -6,4 +6,6 @@ class Product < ActiveRecord::Base
 																						with: %r{\.(gif|jpg|png)\Z}i,
 																						message: 'must be a URL for GIF, JPG or PNG image.'
 																						}
+	has_many :line_items
+
 end
