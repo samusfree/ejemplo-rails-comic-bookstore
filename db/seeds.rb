9ba8e600853084e4c507713a8cce7a6651070223
Julio Lopez Montalvo <ljuliom@gmail.com>
2014-07-24 22:51:09 -0500

    view products index and seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,5 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+Product.create!(title: "Espada de Jon Snow", description: "Consigue el tuyo ahora! Longclaw, la espada de Jon, de Juego de Tronos, se mantuvo fuera de la vista, hasta que la fatídica noche llegó cuando un wight - un hombre muerto traído a la vida por las criaturas de leyenda de pesadilla - trató de matar a Old Bear en su sueño...", image_url: "deadpool.jpg", price: 250.00)
